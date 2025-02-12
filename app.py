e06832666bdc6c47361004cbf9f9f28fcf461faf
deadlyflourish4 <bdanhcse.work@gmail.com>
2025-02-12 16:17:05 +0700

    fix error load model

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from btn_dowload_model import *
-from btn_predict import classifier
+# from btn_predict import classifier
+from transformers import pipeline
 # import
 
 st.title("ML Deployment Here!!!")
-
+download_dir(local_path, s3_prefix)
 btn_download = st.button("Download model")
 if btn_download:
     with st.spinner("Downloading... Pls wait!"):
@@ -14,5 +15,7 @@ text = st.text_area("Enter your review", placeholder="Type here...")
 btn_predict = st.button("Predict")
 if btn_predict:
     with st.spinner("Predicting... Pls wait!"):
+        classifier = pipeline('text-classification', model='tinybert-sentiment-analysis', device='cuda')
+
         ouput = classifier(text)
         st.write(ouput)
